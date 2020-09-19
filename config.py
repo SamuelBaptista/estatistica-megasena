3eb4c46a62fedd47cf5d6c8368f29c19528f09b9
SamuelBaptista <samuelbaptista27@gmail.com>
2020-09-19 10:48:19 -0300

    functions to exploratory analysis

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-SAVE_PATH = r'C:\Users\Samuel\Dropbox\Samuel\Codigos\Github\Estatistica_MegaSena'
 DOWNLOAD_PATH = 'http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip'
 
 FILENAME = 'raffle.html'
--- functions.py
+++ functions.py
@@ -5,7 +5,13 @@ import os
 
 import pandas as pd
 
-from config import SAVE_PATH, DOWNLOAD_PATH, FILENAME
+from config import DOWNLOAD_PATH, FILENAME
+
+
+def get_working_dir_path():
+
+    return os.getcwd()
+
 
 
 def download_raffle_file(url, path, filename):
@@ -22,14 +28,51 @@ def download_raffle_file(url, path, filename):
 
 
 def transform_html_to_csv(path, filename):
-    table = pd.read_html(os.path.join(path, filename))
-    df = pd.DataFrame(table[0])
 
-    df.to_csv(os.path.join(path, os.path.splitext(filename)[0] + '.csv'), index=False)
+    table = pd.read_html(os.path.join(path, filename),
+                         thousands=".",
+                         decimal=",")
+
+    dataframe = pd.DataFrame(table[0])
+
+    dataframe.to_csv(os.path.join(path, os.path.splitext(filename)[0] + '.csv'), index=False)
 
 
+def pre_process_dataframe(filename, **kwargs):
+
+    csv_file = os.path.splitext(filename)[0] + '.csv'
+
+    dataframe = pd.read_csv(csv_file,
+                            index_col='Concurso',
+                            parse_dates=['Data Sorteio'],
+                            na_values='000')
+
+    if 'drop' in kwargs.keys():
+
+        drop_columns = kwargs['drop']
+
+        dataframe.drop(drop_columns,
+                       axis=1,
+                       inplace=True)
+
+
+    dataframe.drop_duplicates(inplace=True)
+    dataframe.fillna(0, inplace=True)
+    
+    dataframe = dataframe.convert_dtypes()
+
+    return dataframe
 
 if __name__ == "__main__":
 
-    download_raffle_file(url=DOWNLOAD_PATH, path=SAVE_PATH, filename=FILENAME)
-    transform_html_to_csv(path=SAVE_PATH, filename=FILENAME)
+    save_path = get_working_dir_path()
+
+    download_raffle_file(url=DOWNLOAD_PATH,
+                         path=save_path,
+                         filename=FILENAME)
+
+
+    transform_html_to_csv(path=save_path,
+                          filename=FILENAME)
+
+    pre_process_dataframe(filename=FILENAME, drop=['Cidade', 'UF'])            
