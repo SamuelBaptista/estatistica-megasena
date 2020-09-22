30b4aba23f40fd64374d21fab1cc50d6497e6af5
SamuelBaptista <samuelbaptista27@gmail.com>
2020-09-22 18:33:11 -0300

    final gambler version

--- src/gambler.py
+++ src/gambler.py
@@ -1,51 +1,90 @@
 import random
-import os
 
-from numpy import mean  
-from time import time
 from tqdm import tqdm
+from numpy import max
+from time import sleep
 
-from threading import Thread
-from threading import Lock
 
+class Gambler:
 
-class Gambler(Thread):
-
-    def __init__(self, numbers_range, numbers_amount, trials, samples):
+    def __init__(self, numbers_range, numbers_amount, numbers_played, trials, samples):
 
         self.numbers_range = numbers_range
         self.numbers_amount = numbers_amount
+        self.numbers_played = numbers_played
+        
         self.trials = trials
         self.samples = samples
 
-        self.lock = Lock()
+        self.hits = []
+        self.hits_list = []
+                
+        self.cheers = False
 
-        self.cpu_count = os.cpu_count()
+    def gamble(self):
+        return (random.sample(range(1, self.numbers_range+1),
+                              self.numbers_played) for _ in range(self.trials))
+         
 
-        super().__init__()
-
-    def run(self):
-        
-        for i in self.samples:
-
-            yield self.gambler()
+    def gamble_samples(self):   
+        return (self.gamble() for _ in range(self.samples))
 
 
-    def gambler(self):
+    @staticmethod
+    def check_hits(raffle_card, raffle_numbers):
+        hits = sum(numbers in raffle_card for numbers in raffle_numbers)
 
-        for i in range(tqdm(self.trials)):
+        return hits
+    
 
-            yield random.sample(range(self.numbers_range), self.numbers_amount)
+    def check_raffle_cards(self, gamble, raffle_numbers):              
+        for raffle_card in tqdm(gamble, total=self.trials):
+            
+            hits = self.check_hits(raffle_card, raffle_numbers)            
+            self.hits.append(hits)
+
+        self.hits_list.append(self.hits.copy())     
             
 
-
-    def check_results(self):
-        pass
-
-
-    def _check_numbers(self):
-        pass
+    def celebrate(self):        
+        if self.numbers_amount in self.hits:
+            self.cheers = True
 
 
-    def _create_report(self):
-        pass
+    def yell(self):
+
+        bold = '\033[1m'
+        clear = '\033[m' 
+        
+        if self.cheers:
+
+            print('YAY! We won!')
+            print(f'We hit all the {bold}{self.numbers_amount}{clear} numbers!')
+
+        else:
+
+            print(f"WOW! Even with {bold}{self.trials:,}{clear} trials,", end=' ')
+            print(f"we didn't hit all the {bold}{self.numbers_amount}{clear} numbers!")
+            print(f'Our best game just hit {bold}{max(self.hits)}{clear} numbers!')
+
+
+    def play(self, raffle_numbers):
+
+        gamble_samples = self.gamble_samples()      
+
+        for i, gamble in enumerate(gamble_samples):
+            sleep(0.5)          
+            self.check_raffle_cards(gamble, raffle_numbers)
+
+            if i == 0:
+                
+                print()       
+                self.celebrate()
+                self.yell()  
+                print()
+                print('-=' * 30, end='\n\n')
+                print("Now, i'll play all the remaining games... Just wait! Thanks.", end='\n\n')
+                
+            
+            self.hits.clear()
+            self.cheers = False
